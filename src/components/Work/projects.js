6b024c5f7b668681124fefdcbada0fa8c393cf16
piusprince <45078705+piusprince@users.noreply.github.com>
2021-11-06 23:33:42 +0000

    Update projects.js
    created a mockup file for all the projects

--- src/components/Work/projects.js
+++ src/components/Work/projects.js
@@ -1,20 +1,30 @@
+import projectImg from '../../images/projectImg.png'
+import bookstore from '../../images/bookstore.png'
 
 export const projects = [
     {
-        id : 1,
-        name : 'Project 1',
-        // img : 'laptop'
+        id: 1,
+        name: 'Project 1',
+        desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Mauris vitae ultricies leo integer malesuada nunc. Enim nec dui nunc mattis enim ut tellus elementum sagittis.',
+        img: projectImg,
+        github: 'https://github.com/piusprince',
+        liveLink: 'https://piusprince.github.io/',
     },
-
     {
-        id : 2,
-        name : 'Project 2',
-        // img = ''
+        id: 2,
+        name: 'Project 2',
+        desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Mauris vitae ultricies leo integer malesuada nunc. Enim nec dui nunc mattis enim ut tellus elementum sagittis.',
+        img: bookstore,
+        github: 'https://github.com/piusprince',
+        liveLink: 'https://piusprince.github.io/',
     },
-
     {
-        id : 3,
-        name : 'Project 3',
-        // img = ''
+        id: 1,
+        name: 'Project 1',
+        desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Mauris vitae ultricies leo integer malesuada nunc. Enim nec dui nunc mattis enim ut tellus elementum sagittis.',
+        img: projectImg,
+        github: 'https://github.com/piusprince',
+        liveLink: 'https://piusprince.github.io/'
     }
+    
 ]
