236100bfd56e48f4138e8fe8e62d621624295769
piusprince <45078705+piusprince@users.noreply.github.com>
2022-01-27 22:23:08 +0000

    Update App.js
    Added code splitting by implementing react lazy and Suspense

--- src/App.js
+++ src/App.js
@@ -1,41 +1,42 @@
-import { BrowserRouter as Router, Switch,  Route } from 'react-router-dom'
-import './App.css';
-import Navbar from './components/Navbar';
-import profile from './images/profile1.webp'
-import HomePage from './pages/HomePage';
-import Footer from './components/Footer';
-import ProjectsPage from './pages/ProjectsPage/ProjectsPage';
-import ServicesPage from './pages/ServicesPage/ServicesPage';
-import AboutPage from './pages/AboutPage/AboutPage';
+import { lazy, Suspense } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import profile from "./images/profile1.webp";
+import HomePage from "./pages/HomePage";
+import Footer from "./components/Footer";
+
+const ProjectsPage = lazy(() => import("./pages/ProjectsPage/ProjectsPage"));
+const ServicesPage = lazy(() => import("./pages/ServicesPage/ServicesPage"));
+const AboutPage = lazy(() => import("./pages/AboutPage/AboutPage"));
 
 function App() {
-  return ( 
-    <Router>
-      <Navbar profile={profile}/>
-      
-      <Switch>
+  return (
+    <Suspense fallback={<h2>Loading page</h2>}>
+      <Router>
+        <Navbar profile={profile} />
 
-        <Route exact path='/'>
-          <HomePage />
-        </Route>
+        <Switch>
+          <Route exact path="/">
+            <HomePage />
+          </Route>
 
-        <Route path='/about'>
-          <AboutPage />
-        </Route>
+          <Route path="/about">
+            <AboutPage />
+          </Route>
 
-        <Route path='/projects'>
-          <ProjectsPage />
-        </Route>
+          <Route path="/projects">
+            <ProjectsPage />
+          </Route>
 
-        <Route path='/services'>
-          <ServicesPage />
-        </Route>
-        
-      </Switch>
+          <Route path="/services">
+            <ServicesPage />
+          </Route>
+        </Switch>
 
-        <Footer profile={profile}/>
-    </Router> 
-  
+        <Footer profile={profile} />
+      </Router>
+    </Suspense>
   );
 }
 
