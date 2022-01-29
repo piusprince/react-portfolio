983023aa851bd57f06b77e68a0a3ddf9b0c9a705
piusprince <45078705+piusprince@users.noreply.github.com>
2022-01-29 15:47:01 +0000

    Update HomePage.js

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,24 +1,23 @@
-import React from 'react'
-import Header from '../components/Header'
-import Work from '../components/Work'
-import Servicess from '../components/Servicess'
-import Testimonial from '../components/Testimonial'
+import React from "react";
+import Header from "../components/Header";
 
-import {projects} from '../components/Work/projects'
-import profile from '../images/profile1.webp'
-import headerSVG from '../images/HeaderSVG.svg'
-import laptop from '../images/laptop.png'
-import mobile from '../images/mobile.png'
+import Work from "../components/Work";
+import Servicess from "../components/Servicess";
+import Testimonial from "../components/Testimonial";
+
+import { projects } from "../components/Work/projects";
+import profile from "../images/profile1.webp";
+import headerSVG from "../images/HeaderSVG.svg";
 
 const HomePage = () => {
-    return (
-        <>
-            <Header headerSVG={headerSVG}/>
-            <Work project={projects} laptop={laptop} mobile={mobile}/>
-            <Servicess />
-            <Testimonial profile={profile}/>
-        </>
-    )
-}
+  return (
+    <>
+      <Header headerSVG={headerSVG} />
+      <Work project={projects} />
+      <Servicess />
+      <Testimonial profile={profile} />
+    </>
+  );
+};
 
-export default HomePage
+export default HomePage;
