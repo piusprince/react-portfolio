52a9ad49ac079270a13f21a29585dca069c1d6be
piusprince <45078705+piusprince@users.noreply.github.com>
2022-01-29 15:50:30 +0000

    Update App.js
    Setup sidebar logic

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { React, useState } from "react";
 import { lazy, Suspense } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
@@ -5,16 +6,29 @@ import Navbar from "./components/Navbar";
 import profile from "./images/profile1.webp";
 import HomePage from "./pages/HomePage";
 import Footer from "./components/Footer";
+import Sidebar from "./components/Sidebar";
 
 const ProjectsPage = lazy(() => import("./pages/ProjectsPage/ProjectsPage"));
 const ServicesPage = lazy(() => import("./pages/ServicesPage/ServicesPage"));
 const AboutPage = lazy(() => import("./pages/AboutPage/AboutPage"));
 
 function App() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleSidebar = () => {
+    setIsOpen(!isOpen);
+    console.log("clicked");
+  };
+
   return (
     <Suspense fallback={<h2>Loading page</h2>}>
       <Router>
-        <Navbar profile={profile} />
+        <Navbar
+          profile={profile}
+          isOpen={isOpen}
+          toggleSidebar={toggleSidebar}
+        />
+        {isOpen && <Sidebar isOpen={isOpen} />}
 
         <Switch>
           <Route exact path="/">
