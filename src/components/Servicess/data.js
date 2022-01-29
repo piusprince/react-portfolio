65e1419a9836919f9c69df41fa9d8baf22bbad91
piusprince <45078705+piusprince@users.noreply.github.com>
2022-01-29 15:47:51 +0000

    Update data.js
    Removed comments in the file

--- src/components/Servicess/data.js
+++ src/components/Servicess/data.js
@@ -1,28 +1,24 @@
-
 export const service1 = {
-    id: 1,
-    title: "Website Development",
-    // logo: '',
-    text: "I build websites to suite your needs, whether small scale or large scale"
-}
+  id: 1,
+  title: "Website Development",
+
+  text: "I build websites to suite your needs, whether small scale or large scale",
+};
 
 export const service2 = {
   id: 2,
   title: "User Interface Design",
-  // logo: '',
-  text: "I design appealing websites that resonate with users"
-}
+  text: "I design appealing websites that resonate with users",
+};
 
 export const service3 = {
   id: 3,
   title: "On Going support",
-  // logo: '',
-  text: "Once there is a need for a new feature, I would always be available to implement it"
-}
+  text: "Once there is a need for a new feature, I would always be available to implement it",
+};
 
 export const service4 = {
   id: 4,
   title: "Effective Testing",
-  // logo: '',
-  text: "Quality is my hallmark, as such I test websites to make sure it does not fall below standards"
-}
+  text: "Quality is my hallmark, as such I test websites to make sure it does not fall below standards",
+};
