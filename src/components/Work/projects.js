b7c4b3f0270550a88be27555749ad8c9409ff654
piusprince <45078705+piusprince@users.noreply.github.com>
2022-02-01 03:22:22 +0000

    Update projects.js
    Improved the description of projects listed

--- src/components/Work/projects.js
+++ src/components/Work/projects.js
@@ -4,20 +4,20 @@ import bookstore from "../../images/bookstore.webp";
 export const projects = [
   {
     id: 1,
-    name: "Project 1",
-    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Mauris vitae ultricies leo integer malesuada nunc. Enim nec dui nunc mattis enim ut tellus elementum sagittis.",
+    name: "Master's Pizza",
+    desc: "A fictional pizza restaurant website with responsive user interface. This website was built with React and Styled Components. New features are being added such as building a login system, adding a cart feature, payment system, and more.",
     img: projectImg,
   },
   {
     id: 2,
-    name: "Project 2",
-    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Mauris vitae ultricies leo integer malesuada nunc. Enim nec dui nunc mattis enim ut tellus elementum sagittis.",
+    name: "Folon Bookstore",
+    desc: "Folon is a bookstore website that would allow its users to browse through a catalog of books, and make purchases. This web application is yet to be built hopefully before the year ends. It would be built with React using typescript, styled-components or tailwindcss, a state management library(redux or recoil), and more.",
     img: bookstore,
   },
   {
     id: 1,
-    name: "Project 3",
-    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Mauris vitae ultricies leo integer malesuada nunc. Enim nec dui nunc mattis enim ut tellus elementum sagittis.",
+    name: "pThoughts",
+    desc: "Ever needed a space where you are surrounded with daily positive thoughts? Well, this is the place for you. This is a personal project that I am currently building. It is a web application that would allow users get daily positive messages from renowned personalities.",
     img: projectImg,
   },
 ];
