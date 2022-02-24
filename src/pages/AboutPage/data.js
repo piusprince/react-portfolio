ff513041a23b89af529a45a021a099322c22c5ad
piusprince <45078705+piusprince@users.noreply.github.com>
2022-02-24 14:48:45 +0000

    Create data.js

---
+++ src/pages/AboutPage/data.js
@@ -0,0 +1,40 @@
+import fm from "../../images/FM22.webp";
+import COD from "../../images/COD.webp";
+import davinci from "../../images/davinci.webp";
+import Halo from "../../images/Halo.webp";
+
+export const data = [
+  {
+    id: 1,
+    title: "Football Manager 22",
+    img: fm,
+  },
+  {
+    id: 2,
+    title: "Halo Infinite",
+    img: Halo,
+  },
+  {
+    id: 3,
+    title: "Call of Duty: Modern Warfare",
+    img: COD,
+  },
+];
+
+export const novels = [
+  {
+    id: 1,
+    title: "The Da Vinci Code",
+    img: fm,
+  },
+  {
+    id: 2,
+    title: "Inferno",
+    img: fm,
+  },
+  {
+    id: 3,
+    title: "Origin",
+    img: fm,
+  },
+];
