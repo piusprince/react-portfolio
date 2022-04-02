dc410190d63e9194a7f97627f3539510655a4173
piusprince <45078705+piusprince@users.noreply.github.com>
2022-04-02 15:45:26 +0000

    Update projects.js
    Fixed conflicting id causing multiple key error

--- src/components/Work/projects.js
+++ src/components/Work/projects.js
@@ -20,7 +20,7 @@ export const projects = [
     live: "https://folon-bookstore.netlify.app/",
   },
   {
-    id: 1,
+    id: 3,
     name: "pThoughts",
     desc: "Ever needed a space where you are surrounded with daily positive thoughts? Well, this is the place for you. This is a personal project that I am currently building. It is a web application that would allow users get daily positive messages from renowned personalities.",
     img: pThought,
