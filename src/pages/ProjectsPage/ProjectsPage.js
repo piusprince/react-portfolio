4c634483147adac6a8a4b4d3fa09c2f9e7dd1436
piusprince <45078705+piusprince@users.noreply.github.com>
2022-04-17 12:45:54 +0000

    Update ProjectsPage.js
    Removed unused import of styled components

--- src/pages/ProjectsPage/ProjectsPage.js
+++ src/pages/ProjectsPage/ProjectsPage.js
@@ -1,7 +1,6 @@
 import React from "react";
 import Work from "../../components/Work";
 import { projects } from "../../components/Work/projects";
-import styled from "styled-components";
 
 const ProjectsPage = () => {
   return <Work projects={projects} />;
